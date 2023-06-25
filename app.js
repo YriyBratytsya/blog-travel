8beb7046e6f5c815d0d97358e8db3bab57f2aa71
Yuriy <ubratica@gmail.com>
2023-06-25 19:19:07 +0300

    add active-menu

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-// let heart = document.querySelectorAll(".heart");
-// console.log(heart);
+let heart = document.querySelectorAll(".hearts");
+
+heart.forEach((items) => {
+  items.addEventListener("click", function () {
+    if (items.classList.contains("heart-active")) {
+      items.classList.remove("heart-active");
+      items.classList.add("hearts");
+    } else {
+      items.classList.add("heart-active");
+      items.classList.remove("hearts");
+    }
+  });
+});
+
+let mobileMenu = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".menu-wrap");
+
+mobileMenu.addEventListener("click", function () {
+  mobileMenu.classList.toggle("active-menu");
+  if (mobileMenu.classList.contains("active-menu")) {
+    mainMenu.classList.add("active-menu");
+  } else {
+    mainMenu.classList.remove("active-menu");
+  }
+});
